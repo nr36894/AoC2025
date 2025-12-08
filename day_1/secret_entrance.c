46d3b799502ad629b00787ad76de09bf4ef5570a
nr36894 <nicholasr@hargray.com>
2025-12-08 14:39:04 -0500

    Much better step 2

--- day_1/secret_entrance.c
+++ day_1/secret_entrance.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int main() {
     FILE *fp;
     char line[6];
     char turn_amt[5];
+    int turn_val;
     int dialPos = 50;
     int direction = 0;
     int password = 0;
     int lineNum = 1;
+    int pw_change;
+    bool triggered_pass = false;
 
     fp = fopen("input.txt", "r");
 
@@ -18,6 +22,9 @@ int main() {
     }
 
     while (fgets(line, sizeof(line), fp) != NULL) {
+        triggered_pass = false;
+        pw_change = 0;
+
         if (line[0] == 'L') {
             direction = -1;
         } else if (line[0] == 'R') {
@@ -31,16 +38,46 @@ int main() {
             turn_amt[i] = '\0';
         }
 
-        for (int i = 0; i < atoi(turn_amt); i++) {
-            dialPos = dialPos + (direction);
-            dialPos = dialPos % 100;
+        turn_val = atoi(turn_amt) * direction;
 
-            if (dialPos == 0) {
+        if (direction < 0) {
+            if (abs(turn_val) >= dialPos && dialPos != 0) {
+                turn_val = turn_val + dialPos;
+                dialPos = 0;
                 password++;
+                pw_change++;
+                triggered_pass = true;
+            }
+        } else if (direction > 0) {
+            if (turn_val >= (100 - dialPos) && dialPos != 0) {
+                turn_val = dialPos - (100 - turn_val);
+                dialPos = 0;
+                password++;
+                pw_change++;
+                triggered_pass = true;
             }
         }
 
-        printf("Line %4d: %5d\n", lineNum, dialPos);
+        int change = abs(turn_val / 100);
+        if (change > 0) {
+            password += change;
+            pw_change += change;
+            triggered_pass = true;
+        }
+        dialPos += turn_val;
+        dialPos = dialPos % 100;
+
+        if (dialPos < 0) {
+            dialPos = 100 + dialPos;
+        }
+
+        // if (dialPos == 0) {
+        //     password++;
+        //     pw_change++;
+        //     triggered_pass = true;
+        // }
+
+        printf("Line %4d: %3d %5s %d\n", lineNum, dialPos, triggered_pass ? "true" : "false", pw_change);
         lineNum++;
     }
 
