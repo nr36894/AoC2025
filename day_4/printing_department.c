d75990172cfc980f1784e9bd240b209f90fd2cd5
nr36894 <nicholasr@hargray.com>
2025-12-16 11:38:40 -0500

    working d4p2

--- day_4/printing_department.c
+++ day_4/printing_department.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <strings.h>
+#include <stdbool.h>
+
+#define TESTING false
 
 int main() {
     FILE *fp;
@@ -8,14 +11,23 @@ int main() {
     int line_length = 0;
     int num_lines = 0;
     int curr_line = 0;
-    int num_accessable = 0;
+    int num_accessable = -1;
+    int total_removed = 0;
     
 
-    fp = fopen("input.txt", "r");
+    if (TESTING) {
+        fp = fopen("test.txt", "r");
+    } else {
+        fp = fopen("input.txt", "r");
+    }
 
     if (!fp) {
         perror("Could not open file.\n");
         return 1;
+    }
+
+    if (TESTING) {
+        printf("Initial state:\n");
     }
 
     while (fgets(line, sizeof(line), fp) != NULL) {
@@ -23,95 +35,121 @@ int main() {
             line_length = strlen(line) - 1;
         }
 
-        for (int i = 0; i < line_length; i++) {
-            printf(" %c ", line[i]);
+        if (TESTING) {
+            for (int i = 0; i < line_length; i++) {
+                printf(" %c ", line[i]);
+            }
+
+            printf("\n");
         }
 
-        printf("\n");
         num_lines++;
     }
 
+    char (*warehouse_map)[num_lines] = malloc(sizeof(int[line_length][num_lines]));
     int (*warehouse)[num_lines] = malloc(sizeof(int[line_length][num_lines]));
-
-    for (int linenum = 0; linenum < num_lines; linenum++) {
-        for (int lineidx = 0; lineidx < line_length; lineidx++) {
-            warehouse[lineidx][linenum] = 0;
-        }
-    }
 
     rewind(fp);
 
     while (fgets(line, sizeof(line), fp) != NULL) {
-        // if (curr_line == 0) {
-        //     for (int lineidx = 1; lineidx < line_length; lineidx++) {
-        //         warehouse[lineidx][0] = lineidx + 1;
-        //     }
-        // }
-
-        // warehouse[0][curr_line] = curr_line + 1;
-        // curr_line++;
         for (int lineidx = 0; lineidx < line_length; lineidx++) {
-            if (line[lineidx] == '.') {
-                warehouse[lineidx][curr_line] = -9999;
-                continue;
-            }
-
-            if (lineidx - 1 >= 0) {
-                warehouse[lineidx - 1][curr_line]++;
-
-                if (curr_line - 1 >= 0) {
-                    warehouse[lineidx - 1][curr_line - 1]++;
-                }
-
-                if (curr_line + 1 < num_lines) {
-                    warehouse[lineidx - 1][curr_line + 1]++;
-                }
-            }
-
-            if (lineidx + 1 < line_length) {
-                warehouse[lineidx + 1][curr_line]++;
-
-                if (curr_line - 1 >= 0) {
-                    warehouse[lineidx + 1][curr_line - 1]++;
-                }
-
-                if (curr_line + 1 < num_lines) {
-                    warehouse[lineidx + 1][curr_line + 1]++;
-                }
-            }
-
-            if (curr_line - 1 >= 0) {
-                warehouse[lineidx][curr_line - 1]++;
-            }
-
-            if (curr_line + 1 < num_lines) {
-                warehouse[lineidx][curr_line + 1]++;
-            }
+            warehouse_map[lineidx][curr_line] = line[lineidx];
         }
         curr_line++;
     }
 
-    printf("\nline_length: %d, num_lines: %d\n\n", line_length, num_lines);
+    fclose(fp);
 
-    for (int linenum = 0; linenum < num_lines; linenum++) {
-        for (int lineidx = 0; lineidx < line_length; lineidx++) {
-            if (warehouse[lineidx][linenum] < 0) {
-                printf("%2c ", '.');
-            } else if (warehouse[lineidx][linenum] < 4) {
-                printf("%2c ", 'x');
-                num_accessable++;
-            } else {
-                printf("%2c ", '@');
+    while (num_accessable != 0) {
+        num_accessable = 0;
+
+        for (int linenum = 0; linenum < num_lines; linenum++) {
+            for (int lineidx = 0; lineidx < line_length; lineidx++) {
+                warehouse[lineidx][linenum] = 0;
             }
         }
+
+        for (curr_line = 0; curr_line < num_lines; curr_line++) {
+            for (int lineidx = 0; lineidx < line_length; lineidx++) {
+                if (warehouse_map[lineidx][curr_line] == '.') {
+                    warehouse[lineidx][curr_line] = -9999;
+                    continue;
+                }
+
+                if (lineidx - 1 >= 0) {
+                    warehouse[lineidx - 1][curr_line]++;
+
+                    if (curr_line - 1 >= 0) {
+                        warehouse[lineidx - 1][curr_line - 1]++;
+                    }
+
+                    if (curr_line + 1 < num_lines) {
+                        warehouse[lineidx - 1][curr_line + 1]++;
+                    }
+                }
+
+                if (lineidx + 1 < line_length) {
+                    warehouse[lineidx + 1][curr_line]++;
+
+                    if (curr_line - 1 >= 0) {
+                        warehouse[lineidx + 1][curr_line - 1]++;
+                    }
+
+                    if (curr_line + 1 < num_lines) {
+                        warehouse[lineidx + 1][curr_line + 1]++;
+                    }
+                }
+
+                if (curr_line - 1 >= 0) {
+                    warehouse[lineidx][curr_line - 1]++;
+                }
+
+                if (curr_line + 1 < num_lines) {
+                    warehouse[lineidx][curr_line + 1]++;
+                }
+            }
+        }
+
+        for (int linenum = 0; linenum < num_lines; linenum++) {
+            for (int lineidx = 0; lineidx < line_length; lineidx++) {
+                if (warehouse[lineidx][linenum] < 4 && warehouse[lineidx][linenum] >= 0) {
+                    num_accessable++;
+                }
+            }
+        }
+
+        printf("\nRemove %d rolls of paper.\n", num_accessable);
+        total_removed += num_accessable;
+
+        for (int linenum = 0; linenum < num_lines; linenum++) {
+            for (int lineidx = 0; lineidx < line_length; lineidx++) {
+                if (warehouse[lineidx][linenum] < 0) {
+                    if (TESTING) {
+                        printf("%2c ", '.');
+                    }
+                } else if (warehouse[lineidx][linenum] < 4) {
+                    if (TESTING) {
+                        printf("%2c ", 'x');
+                    }
+                    warehouse[lineidx][linenum] = -9999;
+                    warehouse_map[lineidx][linenum] = '.';
+                } else {
+                    if (TESTING) {
+                        printf("%2c ", '@');
+                    }
+                }
+            }
+            if (TESTING) {
+                printf("\n");
+            }
+        }
+    }
+
+    if (TESTING) {
         printf("\n");
     }
 
-    printf("\n");
-
-    printf("There are %d rolls accessable.\n", num_accessable);
-
-    fclose(fp);
+    printf("%d rolls of paper can be removed.\n", total_removed);
 
     return 0;
 }
