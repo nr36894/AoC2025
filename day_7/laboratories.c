b1f6dca26e65185b06461e2e58d6161b7af2ad6f
nr36894 <nicholasr@hargray.com>
2025-12-17 15:47:23 -0500

    Theoretically works, it would take till the heatdeath of the universe

--- day_7/laboratories.c
+++ day_7/laboratories.c
@@ -4,6 +4,62 @@
 #include <stdbool.h>
 
 #define TESTING false
+#define GREEN "\033[0;32m"
+#define NO_COL "\033[0m"
+
+int tracePath(int row, int col, int line_length, int num_lines, void* map) {
+    char (*tachyon_map)[num_lines] = map;
+    char (*tmp_map)[num_lines] = NULL;
+
+    if (TESTING) {
+        char (*tmp_map)[num_lines] = malloc(sizeof(char[line_length][num_lines]));
+        memcpy(tmp_map, tachyon_map, sizeof(char[line_length][num_lines]));
+    } else {
+        tmp_map = tachyon_map;
+    }
+
+    int total_paths = 0;
+
+    for (;tachyon_map[col][row] != '^' && row < num_lines; row++) {
+        if (TESTING) {
+            if (tmp_map[col][row] == '.') {
+                tmp_map[col][row] = '|';
+            }
+        }
+    }
+
+    if (row == num_lines) {
+        total_paths++;
+
+        if (TESTING) {
+            for (int curr_row = 0; curr_row < num_lines; curr_row++) {
+                for (int curr_col = 0; curr_col < line_length; curr_col++) {
+                    if (curr_col == col && curr_row == row) {
+                        printf("%s%c%s", GREEN, tmp_map[curr_col][curr_row], NO_COL);
+                    } else {
+                        printf("%c", tmp_map[curr_col][curr_row]);
+                    }
+                }
+                printf("\n");
+            }
+            printf("\n");
+        }
+
+        if (TESTING) {
+            free(tmp_map);
+        }
+        return total_paths;
+    }
+
+    total_paths += tracePath(row, col - 1, line_length, num_lines, tmp_map);
+    total_paths += tracePath(row, col + 1, line_length, num_lines, tmp_map);
+
+    if (TESTING) {
+        free(tmp_map);
+    }
+
+    return total_paths;
+}
 
 int main() {
     FILE *fp;
@@ -11,6 +67,7 @@ int main() {
     int num_lines = 0;
     int line_length = 0;
     int split_count = 0;
+    int s_col = 0;
 
 
     if (TESTING) {
@@ -42,49 +99,14 @@ int main() {
         curr_line++;
     }
 
-    for (int row = 0; row < num_lines; row++) {
-        for (int col = 0; col < line_length; col++) {
-            if (tachyon_map[col][row] == 'S' && tachyon_map[col][row + 1] == '.') {
-                tachyon_map[col][row + 1] = '|';
-            } else if (tachyon_map[col][row] == '^' && tachyon_map[col][row - 1] == '|') {
-                split_count++;
-                if (col + 1 < line_length) {
-                    if (tachyon_map[col + 1][row - 1] == '|') {
-                        tachyon_map[col - 1][row] = '|';
-                    } else if (tachyon_map[col + 1][row] == '.') {
-                        tachyon_map[col + 1][row] = '|';
-                    }
-                }
-                if (col - 1 >= 0) {
-                    if (tachyon_map[col - 1][row - 1] == '|') {
-                        tachyon_map[col - 1][row] = '|';
-                    } else if (tachyon_map[col - 1][row] == '.') {
-                        tachyon_map[col - 1][row] = '|';
-                    }
-                }
-            } else if (tachyon_map[col][row - 1] == '|') {
-                tachyon_map[col][row] = '|';
-            }
-        }
-
-        if (TESTING) {
-            for (int row = 0; row < num_lines; row++) {
-                for (int col = 0; col < line_length; col++) {
-                    printf("%c", tachyon_map[col][row]);
-                }
-                printf("\n");
-            }
-
-            printf("%d Splits.\n\n", split_count);
+    for (int col = 0; col < line_length; col++) {
+        if (tachyon_map[col][0] == 'S') {
+            s_col = col;
+            break;
         }
     }
 
-    for (int row = 0; row < num_lines; row++) {
-        for (int col = 0; col < line_length; col++) {
-            printf("%c", tachyon_map[col][row]);
-        }
-        printf("\n");
-    }
+    split_count = tracePath(0, s_col, line_length, num_lines, tachyon_map);
 
     printf("\n%d Splits.\n", split_count);
 
